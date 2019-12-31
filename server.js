c071ec3e74a3d572548a052d6d53f14b11a30d65
Serhan Ertürk <srnerturk@gmail.com>
2019-12-31 14:11:39 +0300

    server-url

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 var path = require('path');
 var express = require('express');
+const fs = require('fs');
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8081);
+app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function () {
   console.log('listening on port ', server.address().port);
 });
+
+app.get('/getData', function (req, res, next) {
+  let rawdata = fs.readFileSync('data/products.json');
+  let data = JSON.parse(rawdata);
+  res.json(data);
+
+
+});
+
+app.get("/runTestResults", function (request, response) {
+  let rawdata = fs.readFileSync('data/products.json');
+  let data = JSON.parse(rawdata);
+  response.send(data);
+})
